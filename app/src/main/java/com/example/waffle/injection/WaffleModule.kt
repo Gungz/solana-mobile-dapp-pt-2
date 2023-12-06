ef26032c9103c9316c8c42c17dc6c79c94d0d00f
Gungz <gungz87@gmail.com>
2023-12-06 15:26:33 +0700

    Commit for Solana Calyptus Bootcamp

---
+++ app/src/main/java/com/example/waffle/injection/WaffleModule.kt
@@ -0,0 +1,34 @@
+package com.example.waffle.injection
+
+
+import com.metaplex.lib.drivers.rpc.JdkRpcDriver
+import com.solana.mobilewalletadapter.clientlib.MobileWalletAdapter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import com.metaplex.lib.drivers.solana.Commitment
+import com.metaplex.lib.drivers.solana.Connection
+import com.metaplex.lib.drivers.solana.SolanaConnectionDriver
+import com.metaplex.lib.drivers.solana.TransactionOptions
+
+
+@Module
+@InstallIn(
+    ViewModelComponent::class
+)
+class WaffleModule {
+
+
+    @Provides
+    fun providesMobileWalletAdapter(): MobileWalletAdapter {
+        return MobileWalletAdapter()
+    }
+
+    @Provides
+    fun providesMetaplexConnectionDriver(rpcConfig: IRpcConfig): Connection =
+        SolanaConnectionDriver(
+            JdkRpcDriver(rpcConfig.solanaRpcUrl),
+            TransactionOptions(Commitment.CONFIRMED, skipPreflight = true)
+        )
+}
